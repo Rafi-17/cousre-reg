deccd159c99c3da69263abc50dbb65f90b2a6bc1
Rafi <tahmidulrafi13@gmail.com>
2023-09-16 17:45:17 +0600

    Tailwind,vite,google-font all set and Header section added

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+// import PropTypes from 'prop-types';
+
+const Header = () => {
+    return (
+        <header className="max-w-7xl mx-auto m-4 p-4">
+            <h1 className='font-bold text-3xl text-center'>Course Registration</h1>
+        </header>
+    );
+};
+
+export default Header;
